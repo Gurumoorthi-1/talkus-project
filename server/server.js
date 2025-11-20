c75ab249ffd076134b516ca7edff05bcdf4cda07
Gurumoorthi <guru03042005@gmail.com>
2025-11-20 10:12:47 +0530

    server.js update

--- server/server.js
+++ server/server.js
@@ -11,10 +11,9 @@ import Message from "./models/Message.js";
 const app = express();
 const server = http.createServer(app);
 
-// Render allows ANY origin (important)
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
@@ -23,12 +22,13 @@ app.use(express.json({ limit: "4mb" }));
 
 export const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
   },
 });
 
-export const userSocketMap = {};
+export const userSocketMap = {}; 
+
 
 io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
@@ -45,16 +45,17 @@ io.on("connection", (socket) => {
   });
 });
 
-// routes
-app.get("/", (req, res) => res.send("Server is live"));
+
+
+app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
-// connect DB
+// db connection
 await connectDB();
-
-// render uses process.env.PORT
+if(process.env.NODE_env !== "production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Server running on PORT:", PORT));
+}
 
 export default server;
